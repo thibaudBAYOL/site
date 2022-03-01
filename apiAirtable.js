d75c58c9f1270d0e97092caa0ea84b78fbd95862
Thibaud BAYOL <60848400+thibaudBAYOL@users.noreply.github.com>
2022-03-02 00:50:12 +0100

    BookManga + color (moin de consol.log) + timeJS (mode 1 et 2)

--- apiAirtable.js
+++ apiAirtable.js
@@ -1,102 +1,102 @@
-var myapiJS = new ApiClient();
-//Authorization: Bearer 
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer xxx");
-myHeaders.append("Content-Type", "application/json");
+class ApiAirtable {
 
-var DB="xxx";
-var table="xxx";
+     myHeaders= new Headers();
+     myapiJS = new ApiClient();
+     DB="";
+     table="";
 
-/**
- * 
- * @param Array fields (list de field)
- * @returns 
- */
-function getAirtable(fields,filter=null) {
-    url = "https://api.airtable.com/v0/"+DB +
-        "/"+table+"?";
-    if (fields != null) {
-        if (fields.length == 1) {
-            url += "fields[]=" + fields[0];
-        } else if (fields.length > 1) {
-            fields.forEach(element => {
-                url += "fields[]=" + element + "&";
+    constructor(DB_id, tab, mdp) {
+        //Authorization: Bearer 
+        this.myHeaders.append("Authorization", "Bearer " + mdp);
+        this.myHeaders.append("Content-Type", "application/json");
+
+        this.DB = DB_id;
+        this.table = tab;
+    }
+    /**
+     * 
+     * @param Array fields (list de field)
+     * @returns 
+     */
+    getAirtable(fields, filter = null) {
+        var url = "https://api.Airtable.com/v0/" + this.DB +"/" + this.table + "?";
+        if (fields != null) {
+            if (fields.length == 1) {
+                url += "fields[]=" + fields[0];
+            } else if (fields.length > 1) {
+                fields.forEach(element => {
+                    url += "fields[]=" + element + "&";
+                });
+            }
+            if (filter != null) {
+                url += "&";
+            }
+        }
+        if (filter != null) {
+            url += filter;
+        }
+        console.log(url);
+        return this.myapiJS.get(url, this.myHeaders, true)
+    }
+
+
+
+    /**
+     * 
+     * @param Array list (liste de {'fields': { 'field1': 'val1','field2':'val2' }} )
+     * @returns 
+     */
+    addAirtable(list) {
+
+        var url = "https://api.Airtable.com/v0/" + this.DB +
+            "/" + this.table;
+
+        data = JSON.stringify({ "records": list });
+        //data= "{\"records\":[{\"fields\":{\"Name\":\"88\"} } ]}";
+        return this.myapiJS.post(url, data, this.myHeaders, true);
+
+    }
+
+
+    //PATCH  (update)
+    /**
+     * 
+     * @param Array list (liste de {'id':'xxxxxxxxxxx','fields':[{'field1': 'val1'},{'field2': 'val2'}]} )
+     * @returns 
+     */
+    updateAirtable(list) {
+
+
+        var url = "https://api.Airtable.com/v0/" + this.DB +
+            "/" + this.table;
+
+        data = JSON.stringify({ "records": list });
+        //data= "{\"records\":[{\"fields\":{\"Name\":\"88\"} } ]}";
+        console.log(data);
+        return this.myapiJS.patch(url, data, this.myHeaders, true);
+
+    }
+
+
+    /**
+     * 
+     * @param Array list (list des ID a suprimmer)
+     * @returns 
+     */
+    deleteAirtable(list) {
+
+        var url = "https://api.Airtable.com/v0/" + this.DB +
+            "/" + this.table;
+
+        if (list.length == 1) {
+            url += "/" + list[0];
+        } else {
+            url += "?"
+            list.forEach(element => {
+                url += "records[]=" + element + "&";
             });
         }
-        if(filter != null){
-            url+="&";
-        }
-    }
-    if(filter != null){
-        url+=filter;
-    }
-    console.log(url);
-    return myapiJS.get(url, myHeaders, true)
-}
-
-
-
-/**
- * 
- * @param Array list (liste de {'fields': { 'field1': 'val1','field2':'val2' }} )
- * @returns 
- */
-function addAirtable(list) {
-
-
-    url = "https://api.airtable.com/v0/"+DB +
-        "/"+table;
-
-    data = JSON.stringify({ "records": list });
-    //data= "{\"records\":[{\"fields\":{\"Name\":\"88\"} } ]}";
-    console.log(data);
-    return myapiJS.post(url, data, myHeaders, true);
-
-}
-
-
-//PATCH  (update)
-/**
- * 
- * @param Array list (liste de {'id':'xxxxxxxxxxx','fields':[{'field1': 'val1'},{'field2': 'val2'}]} )
- * @returns 
- */
-function updateAirtable(list) {
-
-
-    url = "https://api.airtable.com/v0/"+DB +
-        "/"+table;
-
-    data = JSON.stringify({ "records": list });
-    //data= "{\"records\":[{\"fields\":{\"Name\":\"88\"} } ]}";
-    console.log(data);
-    return myapiJS.patch(url, data, myHeaders, true);
-
-}
-
-
-/**
- * 
- * @param Array list (list des ID a suprimmer)
- * @returns 
- */
-function deleteAirtable(list) {
-
-    var url = "https://api.airtable.com/v0/"+DB +
-        "/"+table;
-
-    if (list.length == 1) {
-        url += "/" + list[0];
-    } else {
-        url += "?"
-        list.forEach(element => {
-            url += "records[]=" + element + "&";
-        });
+        return this.myapiJS.delete(url, null, this.myHeaders, true);
     }
 
-
-    //data= "{\"records\":[{\"fields\":{\"Name\":\"88\"} } ]}";
-
-    //myapiJS.post(url, data, myHeaders, true).then((rep) => { cach = rep; console.log(JSON.stringify(cach)); });
-    return myapiJS.delete(url, null, myHeaders, true);
 }
